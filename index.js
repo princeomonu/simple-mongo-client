dbf33a696c6dc999e9325c7fa43c0da8bb05c614
Prince Omonu <omonuitanyi@gmail.com>
2021-03-06 13:12:05 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const config = require('./dbConfig')
+const { ObjectID } = require('mongodb')
+const jsonfile = require('jsonfile')
+
+const Database = {
+    config: null,
+    db: null,
+    connect: async function (coll){
+        try{
+            console.log('configuring..')
+            this.db = jsonfile.readFileSync('package.json').name
+            this.config = await config(this.db)
+            return new Db(this.config,coll)
+        }catch(err){
+            console.log(err)
+            throw 'couldn not parse json file'
+        }
+    },
+    deleteDatabase: async function(){
+       if(!config) throw 'no database selected'
+       const res = this.config.collection(this.db).drop()
+       return {success: res?true:false}
+    },
+}
+
+
+
+class Db {
+    
+    #collection;
+    constructor(config,coll) {
+        this.#collection = config.collection(coll);
+    }
+    
+    async save(data){
+        if(!data)  throw 'data cannot be undefined'
+        else{
+            const res = await this.#collection.insertOne(data)
+            return {success: res?true:false, _id:  res.insertedId}
+        }
+    }
+
+    async saveMany(data=[]){
+        if(!data)  throw 'data cannot be undefined'
+        else{
+            const res = await this.#collection.insertMany(data)
+            return {success: res?true:false, n:res.result? res.result.n:undefined,  _ids: res.insertedIds}
+        }
+    }
+  
+    async getOne(query={}){
+        const res = (await this.#collection.find(query).toArray())[0]
+        return {success: res?true:false, data: res}
+    }
+
+
+    async search(query={},sort={}){
+        const res = await this.#collection.find(query).sort(sort).toArray()
+        return {success: res?true:false, data: res}
+    }
+
+
+    async getAll(sort={}){
+        const res = await this.#collection.find().sort(sort).toArray()
+        return {success: res?true:false, data: res}
+    }
+
+
+    async update(query,newData){
+        if(!newData)  throw 'data cannot be undefined'
+        else{
+            const savableData = {
+                $set: newData
+            }
+            const res = await this.#collection.updateOne(query,savableData)
+            return {success: res?true:false, n:res.n}
+        }
+    }
+
+    async delete(query){
+        const res = await this.#collection.deleteMany(query)
+        return {success: res?true:false, n:res.n}
+    }
+
+
+    async deleteAll(){
+        const res = await this.#collection.deleteMany()
+        return {success: res?true:false, n:res.n}
+    }
+
+
+    watch(listenFor,callback){
+        this.#collection.watch([
+            {
+                $match: {}
+            }
+        ],{'fullDocument': "updateLookup"}).on("change", function(event) {
+            console.log(JSON.stringify(event));
+            
+            if((listenFor===event.operationType))
+                callback({
+                    documentId: event.documentKey._id,
+                    fullDocument: event.fullDocument,
+                    description: event.updateDescription
+                })
+        });
+    }
+ 
+
+
+}
+
+module.exports = Database
---
+++ index.test.js
@@ -0,0 +1,103 @@
+const Database = require('./index')
+
+describe(':Database',()=>{
+
+    // jest.setTimeout(30000)
+    
+    const mockCollection = 'users'
+    let userDb;
+    let id;
+
+    beforeAll(async()=>{
+        userDb = await Database.connect(mockCollection)
+
+    })
+    
+    const mockUser = {
+        name: 'Prince',
+        gender: 'Male',
+        dob: new Date(),
+        loginUsing: 'Facebook'
+    }
+    
+    const manyMock = [
+        {
+            name: 'Prince',
+            gender: 'Male',
+            dob: new Date(),
+            loginUsing: 'Facebook'
+        },
+        {
+            name: 'Jamiu',
+            gender: 'Male',
+            dob: new Date(),
+            loginUsing: 'Instagram'
+        }
+    ]
+    
+    // //watch collection (insert)
+    // userDb.watch('insert',(response)=>{
+    //     expect(response).toBe({success:true})
+    // });
+    it('should save one item to db', async () => {
+        
+        //save data to collection
+        const response = await userDb.save(mockUser);
+        id = response._id
+        expect(response.success).toBe(true);
+    });
+    
+    it('should save many item to db', async () => {
+        //save many data to collection
+        const response7 = await userDb.saveMany(manyMock);
+        expect(response7.success).toBe(true);
+        
+    });
+    
+    it('should get one item from db', async () => {
+        //get one data from collection
+        const response1 = (await userDb.getOne({_id:id})).data;
+        expect(response1.name).toBe(mockUser.name);
+    });
+    
+    it('should search items from db', async () => {
+        //search for data in collection
+        const response2 = (await userDb.search({_id:id})).data[0];
+        expect(response2.name).toBe(mockUser.name);
+    });
+    
+    it('should get all items from db', async () => {
+        
+        //get all data from collection
+        const response3 = (await userDb.getAll()).data[0]    
+        expect(response3.name).toBe(mockUser.name);
+    });
+    
+    it('should update one item in db', async () => {
+        
+        //update data in collection
+        await userDb.update({_id:id},{name:'James'});
+        //fetch updated
+        const response4 = (await userDb.getOne({_id:id})).data;
+        expect(response4.name).toBe('James');
+    });
+    
+    it('should delete one item from db', async () => {
+        
+        //delete data in collection
+        await userDb.delete({name:'James'});
+        const response5 = (await userDb.getOne({_id:id})).data;
+        expect(response5).toBeUndefined();
+    });
+        
+    it('should delete all items db', async () => {
+        
+        await userDb.save(mockUser);
+        await userDb.deleteAll();
+        const response6 = (await userDb.getOne({_id:id})).data;
+        expect(response6).toBeUndefined();
+        
+    });
+
+
+})
